c01ad8765434ab5c89efa3d47b3be74311a15711
vv221 <vv221@dotslashplay.it>
2017-07-28 19:04:10 +0200

    2.0 - Indiana Jones and the Fate of Atlantis - Use different package name for English and French versions

--- play.it-2/games/play-indiana-jones-and-the-fate-of-atlantis.sh
+++ play.it-2/games/play-indiana-jones-and-the-fate-of-atlantis.sh
@@ -34,27 +34,28 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170727.1
+script_version=20170728.1
 
 # Set game-specific variables
 
 GAME_ID='indiana-jones-and-the-fate-of-atlantis'
 GAME_NAME='Indiana Jones and the Fate of Atlantis'
 
-ARCHIVES_LIST='ARCHIVE_GOG ARCHIVE_GOG_FRENCH'
+ARCHIVES_LIST='ARCHIVE_GOG_EN ARCHIVE_GOG_FR'
 
-ARCHIVE_GOG='gog_indiana_jones_and_the_fate_of_atlantis_2.2.0.27.sh'
-ARCHIVE_GOG_MD5='d56a5850b9d1d4f82eebc282ef7fb52b'
-ARCHIVE_GOG_SIZE='250000'
-ARCHIVE_GOG_VERSION='1.0-gog2.2.0.27'
+ARCHIVE_GOG_EN='gog_indiana_jones_and_the_fate_of_atlantis_2.2.0.27.sh'
+ARCHIVE_GOG_EN_MD5='d56a5850b9d1d4f82eebc282ef7fb52b'
+ARCHIVE_GOG_EN_SIZE='250000'
+ARCHIVE_GOG_EN_VERSION='1.0-gog2.2.0.27'
 
-ARCHIVE_GOG_FRENCH='gog_indiana_jones_and_the_fate_of_atlantis_french_2.2.0.27.sh'
-ARCHIVE_GOG_FRENCH_MD5='0da3a8b3d0505c86a9f863c27debbca7'
-ARCHIVE_GOG_FRENCH_SIZE='99000'
-ARCHIVE_GOG_FRENCH_VERSION='1.0-gog2.2.0.27'
+ARCHIVE_GOG_FR='gog_indiana_jones_and_the_fate_of_atlantis_french_2.2.0.27.sh'
+ARCHIVE_GOG_FR_MD5='0da3a8b3d0505c86a9f863c27debbca7'
+ARCHIVE_GOG_FR_SIZE='99000'
+ARCHIVE_GOG_FR_VERSION='1.0-gog2.2.0.27'
 
 ARCHIVE_DOC_PATH='data/noarch/docs'
 ARCHIVE_DOC_FILES='./*.pdf ./*.txt'
+
 ARCHIVE_GAME_PATH='data/noarch/data'
 ARCHIVE_GAME_FILES='./atlantis.000 ./atlantis.001 ./monster.sou'
 
@@ -65,6 +66,10 @@ APP_MAIN_ICON_RES='256x256'
 
 PACKAGES_LIST='PKG_MAIN'
 
+PKG_MAIN_ID="$GAME_ID"
+PKG_MAIN_ID_GOG_EN="${GAME_ID}-en"
+PKG_MAIN_ID_GOG_FR="${GAME_ID}-fr"
+PKG_MAIN_PROVIDE="$PKG_MAIN_ID"
 PKG_MAIN_DEPS_DEB='scummvm'
 PKG_MAIN_DEPS_ARCH='scummvm'
 
@@ -92,7 +97,7 @@ fi
 extract_data_from "$SOURCE_ARCHIVE"
 tolower "$PLAYIT_WORKDIR/gamedata"
 
-organize_data 'DOC'       "$PATH_DOC"
+organize_data 'DOC'  "$PATH_DOC"
 organize_data 'GAME' "$PATH_GAME"
 
 PATH_ICON="$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
